b5ec099c6fc340b43f2b7c0e96cca1f8205fcdd6
Lazy_Owl <slimsly11@gmail.com>
2020-04-23 17:19:30 +0600

    Выключил автокомплит

--- anime/apps/watch/forms.py
+++ anime/apps/watch/forms.py
@@ -4,3 +4,4 @@ class XSearchForm(forms.Form):
     query = forms.CharField(label = 'Поиск...', min_length = 4, max_length = 255)
 
     query.widget.attrs.update({'class': 'form-control x-search-input'})
+    query.widget.attrs.update({'autocomplete': 'off'})
