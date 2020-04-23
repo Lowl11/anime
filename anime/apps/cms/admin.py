1a57961685b69baf896e681fc1cc5c4194d34bb4
Lazy_Owl <slimsly11@gmail.com>
2020-04-23 21:15:32 +0600

    Добавлена модель ссылок навигационной панели

--- anime/apps/cms/admin.py
+++ anime/apps/cms/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CmsNavigationLinks
+
+admin.site.register(CmsNavigationLinks)
--- anime/apps/cms/models.py
+++ anime/apps/cms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class CmsNavigationLinks(models.Model):
+    name = models.CharField(max_length = 255, null = False)
+    url = models.CharField(max_length = 999, null = False)
+    order_number = models.IntegerField(default = 0)
+
+    class Meta:
+        verbose_name = 'Навигационная ссылка CMS'
+        verbose_name_plural = 'Навигационные ссылки CMS'
+
+    def __str__(self):
+        return self.name + ' [' + str(self.order_number) + ']'
