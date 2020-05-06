eb9638fe73ca0995fa32e3439d5289a34c629e09
Lazy_Owl <slimsly11@gmail.com>
2020-05-06 23:16:38 +0600

    Полностью переписал логику (код) FileManager.js

--- anime/apps/cms/static/cms/js/fm.js
+++ anime/apps/cms/static/cms/js/fm.js
@@ -1,184 +1,243 @@
-function FileManager () {
+class FileManager {
 
-    this.animation = new FMAnimations();
-    this.createFolderUrl = '/cms/fm/create_folder/';
-    this.renameFolderUrl = '/cms/fm/rename_folder/';
-    this.parentId = 0;
-    this.backId = 0;
-    this.folderName = 'Кореная папка';
-    this.objects = [];
+    GUI = new FileManagerGUI();
+    Data = new FileManagerData(this.GUI);
 
-    this.Constructor = function() {
-        $('#folder-name').text(this.folderName);
-        this.OpenFolder();
+    RootFolder = true;
+    CurrentFolderName = 'Коренная папка';
+    FileObjects = [];
+
+    constructor() { 
+        this.GUI.SetCurrentFolder(this.CurrentFolderName, 0);
+        this.FileObjects = this.Data.GetObjects();
     }
 
-    this.BindActions = function() {
-        // elements
-        let createFolder = $('#create-folder');
-        let folders = $('.object.folder');
-        let backFolder = $('#back-folder');
-        let renameFolder = $('#rename-folder');
-        let deleteFolder = $('#delete-folder');
-        let uploadFile = $('#upload-file');
+}
 
-        // binds
-        createFolder.off('click.CreateFolder');
-        createFolder.on('click.CreateFolder', () => {
-            let modalTitle = 'Создать папку';
-            let onApply = (errorObject) => {
-                let folderName = $('#modal-input-input').val();
-                if (folderName.length == 0) {
-                    errorObject.text('Поле для заполнения пустое!');
-                } else { // валидация пройдена :D
-                    errorObject.text('');
-                    let onSuccess = (data) => {
-                        this.animation.CloseModalInput();
-                        this.OpenFolder();
+class FileManagerGUI {
+
+    Data = new FileManagerData(this);
+
+    // Кнопки
+    BackFolder = $('#back-folder');
+    CreateFolder = $('#create-folder');
+    RenameFolder = $('#rename-folder');
+    DeleteFolder = $('#delete-folder');
+    UploadFile = $('#upload-file');
+
+    // модалки
+    ModalInput = $('.modal-input');
+    ModalInputTitle = this.ModalInput.find('.header').find('.title');
+    ModalInputField = this.ModalInput.find('.content').find('input');
+    
+    // "принять" и "закрыть" кнопки модалки
+    ModalApply = $('.apply-modal');
+    ModalCancel = $('.close-modal');
+
+    // Объект текста ошибки
+    ErrorObject = $('.error-msg');
+
+    // Название нынешней папки
+    CurrentFolder = $('#folder-name');
+
+    // div в котором лежат все объекты (файлы, папки)
+    Objects = $('.file-objects');
+
+    constructor() {
+        this.BindActions();
+    }
+
+    BindActions() {
+        // закрытие модалки
+        this.ModalCancel.off('click.ModalCancel');
+        this.ModalCancel.on('click.ModalCancel', () => this.HideModal());
+
+        // кнопка назад
+        this.BackFolder.off('click.BackFolder');
+        this.BackFolder.on('click.BackFolder', () => {
+            let breadCrump = this.GetBreadCrump();
+            let parentId = parseInt(breadCrump[breadCrump.length - 2]);
+            breadCrump.pop();
+            this.SetBreadCrump(breadCrump);
+
+            if (parentId < 0)
+                parentId = 0;
+            this.Data.GetObjects(parentId);
+        });
+
+        // создать папку
+        this.CreateFolder.off('click.CreateFolder');
+        this.CreateFolder.on('click.CreateFolder', () => {
+            let onApply = () => {
+                let folderName = this.ModalInputField.val();
+                if (folderName.length > 0) {
+                    let currentFolderId = this.CurrentFolder.data('id');
+                    let onSuccess = () => {
+                        this.HideModal();
+                        this.Data.GetObjects(currentFolderId);
                     };
-                    this.SendAjax(this.createFolderUrl, { 'name': folderName, 'parent_id': this.parentId }, onSuccess, 'GET');
+                    this.Data.CreateFolder(currentFolderId, folderName, onSuccess);
+                } else {
+                    this.ErrorObject.text('Название папки не может быть пустым');
                 }
-            }
-            this.animation.OpenModalInput(modalTitle, onApply);
+            };
+            this.ShowModal('input', 'Создать папку', onApply);
         });
 
-        folders.off('click.OpenFolder');
-        folders.on('click.OpenFolder', (e) => {
-            this.parentId = $(e.currentTarget).data('id');
-            this.folderName = $(e.currentTarget).data('name');
-            $('#folder-name').text(this.folderName);
-            if (this.parentId != 0 && this.backId != 0)
-                this.backId = this.parentId;
-            this.OpenFolder();
-        });
-
-        backFolder.off('click.BackFolder');
-        backFolder.on('click.BackFolder', (e) => {
-            $('#folder-name').text($(e.currentTarget).data('name'));
-            this.parentId = this.backId;
-            this.OpenFolder();
-        });
-
-        renameFolder.off('click.RenameFolder');
-        renameFolder.on('click.RenameFolder', () => {
-            let modalTitle = 'Переименовать папку';
-            let onApply = (errorObject) => {
-                let folderName = $('#modal-input-input').val();
-                if (folderName.length == 0) {
-                    errorObject.text('Поле для заполнения пустое!');
-                } else { // валидация пройдена :D
-                    errorObject.text('');
-                    let onSuccess = (data) => {
-                        this.animation.CloseModalInput();
-                        this.OpenFolder();
+        this.RenameFolder.off('click.RenameFolder');
+        this.RenameFolder.on('click.RenameFolder', () => {
+            let onApply = () => {
+                let folderName = this.ModalInputField.val();
+                if (folderName.length > 0) {
+                    let currentFolderId = this.CurrentFolder.data('id');
+                    let onSuccess = () => {
+                        this.HideModal();
+                        this.SetCurrentFolder(folderName, currentFolderId);
+                        this.Data.GetObjects(currentFolderId);
                     };
-                    this.SendAjax(this.renameFolderUrl, { 'name': folderName, 'folder_id': this.parentId }, onSuccess, 'GET');
+                    this.Data.RenameFolder(currentFolderId, folderName, onSuccess);
+                } else {
+                    this.ErrorObject.text('Название папки не может быть пустым');
                 }
-            }
-            this.animation.OpenModalInput(modalTitle, onApply, this.folderName);
-        });
-
-        deleteFolder.off('click.DeleteFolder');
-        deleteFolder.on('click.DeleteFolder', () => {
-            console.log('delete folder');
-        });
-
-        uploadFile.off('click.UploadFile');
-        uploadFile.on('click.UploadFile', () => {
-            console.log('upload file');
+            };
+            this.ShowModal('input', 'Переименовать папку', onApply, this.CurrentFolder.text());
         });
     }
 
-    this.OpenFolder = function() {
-        let updateUrl = '/cms/fm/objects/';
-        let onSuccess = (data) => {
-            this.objects = JSON.parse(data);
-            this.UpdateFileManager();
-            this.BindActions();
-        };
-        this.SendAjax(updateUrl, { 'parent_id': this.parentId }, onSuccess, 'GET');
-    }
-
-    this.UpdateFileManager = function() {
-        let fileObjects = $('.file-objects');
+    DrawObjects(objects) {
         let html = '';
-        $.each(this.objects, (index, value) => {
+        $.each(objects, (index, value) => {
             html += '<div class="object-wrapper">';
             html += '<div class="object ' + value.type + '" data-id="' + value.id + '" data-name="' + value.name + '">'
             html += '<div class="glyphicon glyphicon-' + value.type + '-open"></div>';
             html += value.name;
             html += '</div></div>';
         });
-        fileObjects.html(html);
-    }
+        this.Objects.html(html);
 
-    this.SendAjax = function(url, data, onSuccess, requestType = 'POST') {
-        $.ajax({
-            type: requestType,
-            url: url,
-            data: data,
-            cached: false,
-            success: onSuccess,
-            error: function() {
-                console.error('Ошибка запроса Ajax');
-            }
+        let generated = $('.object');
+        generated.off('click.OpenFolder');
+        generated.on('click.OpenFolder', (e) => {
+            let folder = $(e.currentTarget);
+            let folderId = folder.data('id');
+            let folderName = folder.data('name');
+
+            this.SetCurrentFolder(folderName, folderId);
+            let breadCrump = this.GetBreadCrump();
+            breadCrump.push(folderId);
+            this.SetBreadCrump(breadCrump);
+
+            this.Data.GetObjects(folderId);
         });
     }
 
-    this.Constructor();
+    SetCurrentFolder(folderName, folderId) {
+        this.CurrentFolder.text(folderName);
+        this.CurrentFolder.data('id', folderId);
+    }
+
+    ShowModal(type, title, onClick, value = '') {
+        let modalObject, titleObject;
+        switch (type) {
+            case 'input':
+                modalObject = this.ModalInput;
+                titleObject = this.ModalInputTitle;
+                this.ModalInputField.val(value);
+                break;
+            default:
+                modalObject = null;
+                titleObject = null;
+        }
+        modalObject.show();
+        titleObject.text(title);
+        this.ModalApply.off('click.ModalApply');
+        this.ModalApply.on('click.ModalApply', () => onClick(this.ErrorObject));
+        this.ToggleBlur();
+    }
+
+    HideModal() {
+        this.ModalInput.hide();
+        this.ToggleBlur();
+    }
+
+    ToggleBlur() {
+        let blurObject = $('.background-cover');
+        if (blurObject.data('toggled') == 0) {
+            blurObject.show();
+            blurObject.data('toggled', '1');
+        }
+        else {
+            blurObject.hide();
+            blurObject.data('toggled', '0');
+        }
+    }
+
+    GetBreadCrump() {
+        let breadCrumpObject = $('.bread-crump');
+        let breadCrump = breadCrumpObject.text().split(' ');
+        return breadCrump;
+    }
+
+    SetBreadCrump(breadCrump) {
+        let text = '';
+        for (let i = 0; i < breadCrump.length; i++) {
+            text += breadCrump[i] + ' ';
+        }
+        $('.bread-crump').text(text.trim());
+    }
 
 }
 
-function FMAnimations() {
+class FileManagerData {
 
-    this.Constructor = function() {}
+    GUI = null;
+    BaseUrl = '/cms/fm/';
+    CreateFolderUrl = this.BaseUrl + 'create_folder/';
+    RenameFolderUrl = this.BaseUrl + 'rename_folder/';
+    GetObjectsUrl = this.BaseUrl + 'objects/';
 
-    this.OpenModalInput = function(title, onApply, defaultValue = '') {
-        let modalInput = $('.modal-input'); // модалка
+    constructor(gui) {
+        this.GUI = gui;
+    }
 
-        // верх модалки (header)
-        let headerObject = modalInput.find('.header');
-        let titleObject = headerObject.find('.title');
-        
-        // нижняя часть (footer)
-        let footerObject = modalInput.find('.footer');
-        let applyObject = footerObject.find('.apply-modal');
-        let closeObject = footerObject.find('.close-modal');
-        let errorObject = footerObject.find('.error-msg');
+    GetObjects(parentId) {
+        let data = {
+            'parent_id': parentId
+        }
+        let onSuccess = (data) => {
+            let objects = JSON.parse(data);
+            this.GUI.DrawObjects(objects);
+        }
+        this.SendAjax(this.GetObjectsUrl, data, onSuccess);
+    }
 
-        // затемненный фон
-        let backgroundCover = $('.background-cover');
-        backgroundCover.show();
+    CreateFolder(parentId, folderName, onSuccess) {
+        let data = {
+            'name': folderName,
+            'parent_id': parentId
+        }
+        this.SendAjax(this.CreateFolderUrl, data, onSuccess);
+    }
 
-        // инпут
-        let input = modalInput.find('input');
-        input.val(defaultValue);
+    RenameFolder(folderId, folderName, onSuccess) {
+        let data = {
+            'name': folderName,
+            'folder_id': folderId
+        }
+        this.SendAjax(this.RenameFolderUrl, data, onSuccess);
+    }
 
-        titleObject.text(title); // ставим название модалки
-
-        // действие на подтверждение модалки
-        applyObject.off('click.ApplyModalInput');
-        applyObject.on('click.ApplyModalInput', () => {
-            onApply(errorObject);
+    SendAjax(url, data, onSuccess) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            cache: false,
+            success: onSuccess,
+            error: function() {
+                console.error('Ошибка отправки Ajax запроса');
+            }
         });
-
-        // действие на закрытие модалки
-        closeObject.off('click.CloseModalInput');
-        closeObject.on('click.CloseModalInput', () => this.CloseModalInput());
-
-        modalInput.show();
     }
-
-    this.CloseModalInput = function() {
-        let modalInput = $('.modal-input'); // модалка
-        let backgroundCover = $('.background-cover');
-
-        $('#modal-input-input').val(''); // очищение поля
-
-        modalInput.hide();
-        backgroundCover.hide();
-    }
-    
-    this.Constructor();
 
 }
