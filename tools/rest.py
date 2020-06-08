696273501f4b4a46e13e63b425b6e59139f74b6f
Lazy_Owl <slimsly11@gmail.com>
2020-06-09 00:33:18 +0600

    Мини рефакторинг

--- tools/rest.py
+++ tools/rest.py
@@ -4,29 +4,27 @@ import os
 
 from tools import utils
 
-'''
-    REST - тулза которая помогает отправить запрос по урлу
-'''
+""" REST - тулза которая помогает отправить запрос по урлу """
 
-# делает запрос по урлу
-def make_request(url, data, request_type, headers = None):
-    if headers is None: # по дефолту будем отправлять JSON
+
+def make_request(url, data, request_type, headers=None):
+    """ делает запрос по урлу """
+    if headers is None:  # по дефолту будем отправлять JSON
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
     # try/except потому что нужно будет отловить ошибку и логировать (или выкинуть)
     try:
-        
-        if request_type == 'POST':
-            response = requests.post(url, data = data, headers = headers)
-        elif request_type == 'GET':
-            response = requests.get(url, data = data, headers = headers)
-        elif request_type == 'PUT':
-            response = requests.put(url, data = data, headers = headers)
-        elif request_type == 'DELETE':
-            response = requests.delete(url, data = data, headers = headers)
 
-        return response
+        if request_type == 'POST':
+            response = requests.post(url, data=data, headers=headers)
+        elif request_type == 'GET':
+            response = requests.get(url, data=data, headers=headers)
+        elif request_type == 'PUT':
+            response = requests.put(url, data=data, headers=headers)
+        elif request_type == 'DELETE':
+            response = requests.delete(url, data=data, headers=headers)
+
     except Exception as error:
         utils.raise_exception(error)
-    
-    return None
+
+    return response
