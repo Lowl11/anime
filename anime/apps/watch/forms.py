0b4031947bfbca006c966a98da77c186f1a8c852
Lazy_Owl <slimsly11@gmail.com>
2020-06-15 18:23:43 +0600

    Убран лейбл Поиск и добавлен плейсхолдер

--- anime/apps/watch/forms.py
+++ anime/apps/watch/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
+
 class XSearchForm(forms.Form):
-    query = forms.CharField(label = 'Поиск...', min_length = 4, max_length = 255)
+    query = forms.CharField(label = '', min_length = 4, max_length = 255)
 
     query.widget.attrs.update({'class': 'form-control x-search-input'})
     query.widget.attrs.update({'autocomplete': 'off'})
+    query.widget.attrs.update({'placeholder': 'Поиск...'})
