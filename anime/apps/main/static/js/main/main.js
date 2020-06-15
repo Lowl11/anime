da9b045949f4f28f4b110c9ebc25648b4bd48b4d
Lazy_Owl <slimsly11@gmail.com>
2020-06-15 18:20:55 +0600

    Поисковой запрос происходит через js (jquery)

--- anime/apps/main/static/js/main/main.js
+++ anime/apps/main/static/js/main/main.js
@@ -1,3 +1,14 @@
 $(() => {
-    Utils.print('Hello World')
+
+    let xsearch = $('.xsearch input');
+    xsearch.off('keyup.XSearch');
+    xsearch.on('keyup.XSearch', (e) => {
+        if (e.which == 13) {
+            let query = xsearch.val();
+            if (query.length >= 4) {
+                window.location = "/watch/xsearch/?query=" + query;
+            }
+        }
+    });
+
 });
